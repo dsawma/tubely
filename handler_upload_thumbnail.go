bacd048e38d5280c968bf84787d7e7e3eb2e38b7
dsawma <davidthanga13@gmail.com>
2025-12-18 10:55:39 -0500

    first

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -32,6 +34,42 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return 
+
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type", err)
+		return 
+
+	}
+	imageData,err  := io.ReadAll(file)
+	if err != nil{
+		respondWithError(w, http.StatusBadRequest, "Couldn't read", err)
+		return 
+	}
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil{
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return 
+	}
+
+	encodedStr := base64.StdEncoding.EncodeToString(imageData)
+	dataURL := fmt.Sprintf("data:%v;base64,%v", mediaType, encodedStr)
+	videoData.ThumbnailURL = &dataURL
+
+	cfg.db.UpdateVideo(videoData)
+	if err != nil{
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return 
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
 }
